3570b9b2cc6e4ed9f18e1610af8730d610ebfb62
sv8587 <sv8587@srmist.edu.in>
2026-03-17 13:06:03 +0530

    Coded UC1: Display Hello World in console

--- src/main/java/HelloApp.java
+++ src/main/java/HelloApp.java
@@ -1,11 +1,5 @@
 public class HelloApp {
     public static void main(String[] args) {
-
-        if (args.length > 0) {
-            String name = args[0];
-            System.out.println("Hello, " + name + "!");
-        } else {
-            System.out.println("Hello, World!");
-        }
-
+        System.out.println("Hello World");
     }
+}
