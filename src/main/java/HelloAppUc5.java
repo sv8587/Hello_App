0ed5da62ac0f0f3d2a2d9dbadf04fe98f6a8b1fd
sv8587 <sv8587@srmist.edu.in>
2026-03-26 12:34:19 +0530

    Update HelloAppUc5.java

---
+++ src/main/java/HelloAppUc5.java
@@ -0,0 +1,23 @@
+public class HelloApp {
+    public static void main(String[] args) {
+
+        String name = "World";
+
+        if (args.length > 0) {
+            StringBuilder nameBuilder = new StringBuilder();
+            boolean first = true;
+
+            for (String n : args) {
+                if (!first) {
+                    nameBuilder.append(", ");
+                }
+                nameBuilder.append(n);
+                first = false;
+            }
+
+            name = nameBuilder.toString();
+        }
+
+        System.out.println("Hello, " + name + "!");
+    }
+}
